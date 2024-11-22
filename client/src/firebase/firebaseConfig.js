961827ba7d6e5f9a997fdaa7010905352d23b9a4
Dnyaneshwar Patange <114939884+dnyaneshwarpatange@users.noreply.github.com>
2024-11-22 14:06:08 +0530

    Update firebaseConfig.js

--- client/src/firebase/firebaseConfig.js
+++ client/src/firebase/firebaseConfig.js
@@ -4,15 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Firebase configuration using environment variables
-const firebaseConfig = {
-  apiKey: "AIzaSyBPUdDFl4nPIqEEw9iXMqi5pIyOXZNiKUo",
-  authDomain: "drive-e8e69.firebaseapp.com",
-  databaseURL: "https://drive-e8e69-default-rtdb.firebaseio.com",
-  projectId: "drive-e8e69",
-  storageBucket: "drive-e8e69.appspot.com",
-  messagingSenderId: "842498275577",
-  appId: "1:842498275577:web:0d722f7ea41b25e56b8a28"
-};
+
 
 
 // Initialize Firebase app
